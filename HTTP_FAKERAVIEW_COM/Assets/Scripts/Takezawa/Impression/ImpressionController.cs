f932d254fda19fe49336602728c17e77c45931be
ShumaTAkezawa <aesculapius2528@gmail.com>
2019-05-27 14:53:59 +0900

    感想データを簡単に追加できるようにした。多分...

--- HTTP_FAKERAVIEW_COM/Assets/Scripts/Takezawa/Impression/ImpressionController.cs
+++ HTTP_FAKERAVIEW_COM/Assets/Scripts/Takezawa/Impression/ImpressionController.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
+﻿////////////////////////
+// 移動感想の移動関係 //
+////////////////////////
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ImpressionController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    ImpressionGenerate gen = new ImpressionGenerate();
+
+    //Start is called before the first frame update
     void Start()
     {
-        
+        gen = gameObject.GetComponent<ImpressionGenerate>();
+        if (gen.GenerateImpression(0)) { Debug.Log("生成"); }
+        else { Debug.Log("失敗"); }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
---
+++ HTTP_FAKERAVIEW_COM/Assets/Scripts/Util/Tags.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shared
+{
+    public enum Tags
+    {
+        EAT,
+        DRINK,
+        BOOK,
+        STADIE,
+        TOOL,
+        SLAVE,
+        PETS,
+        DRAG,
+        DENGER,
+
+        NON,
+    }
+}
